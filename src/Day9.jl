ca9f390350ae45b465a7ac5c37f65d49b1fde2f1
Shawn LeMaster <shlemas@icloud.com>
2022-12-11 23:57:40 -0600

    More Day 9 refactoring

--- src/Day9.jl
+++ src/Day9.jl
@@ -1,55 +1,64 @@
 module Day9
 
+mutable struct Knot
+    x::Int
+    y::Int
+end
+
 function read_moves(filename)
     lines = readlines(filename)
     moves = map(split, lines)
     return map(v -> (v[1], parse(Int8, v[2])), moves)
 end
 
+function drag_knot!(knot::Knot, direction::AbstractString)
+    if direction == "U"
+        knot.y += 1
+    elseif direction == "D"
+        knot.y -= 1
+    elseif direction == "R"
+        knot.x += 1
+    elseif direction == "L"
+        knot.x -= 1
+    end
+end
+
+function drag_knot!(knot::Knot, target::Knot)
+    drag_up    = ((target.y - knot.y) >=  2)
+    drag_down  = ((target.y - knot.y) <= -2)
+    drag_right = ((target.x - knot.x) >=  2)
+    drag_left  = ((target.x - knot.x) <= -2)
+
+    if drag_up
+        knot.y += 1
+    elseif drag_down
+        knot.y -= 1
+    elseif drag_left || drag_right
+        knot.y = target.y
+    end
+
+    if drag_right
+        knot.x += 1
+    elseif drag_left
+        knot.x -= 1
+    elseif drag_up || drag_down
+        knot.x = target.x
+    end
+end
+
 function pull_rope(moves, num_knots)
-    knots = [[1,1] for _ in 1:num_knots]
-    tail_history = Set([Tuple([1,1])])
+    knots = [Knot(1,1) for _ in 1:num_knots]
+    tail_history = Set([(1,1)])
     for move in moves
-        dir, amount = move
+        direction, amount = move
         while amount > 0
-            if dir == "U"
-                knots[1][1] += 1
-            elseif dir == "D"
-                knots[1][1] -= 1
-            elseif dir == "R"
-                knots[1][2] += 1
-            elseif dir == "L"
-                knots[1][2] -= 1
-            end
+            drag_knot!(knots[1], direction)
             for i in eachindex(knots)
                 if i == lastindex(knots)
-                    push!(tail_history, Tuple(knots[i]))
+                    push!(tail_history, (knots[i].x, knots[i].y))
                     break
                 end
-
-                vertical_diff   = knots[i][1] - knots[i+1][1]
-                horizontal_diff = knots[i][2] - knots[i+1][2]
-
-                pull_up    = (vertical_diff   >=  2)
-                pull_down  = (vertical_diff   <= -2)
-                pull_right = (horizontal_diff >=  2)
-                pull_left  = (horizontal_diff <= -2)
-
-                if pull_up
-                    knots[i+1][1] += 1
-                elseif pull_down
-                    knots[i+1][1] -= 1
-                elseif pull_left || pull_right
-                    knots[i+1][1] = knots[i][1]
-                end
-
-                if pull_right
-                    knots[i+1][2] += 1
-                elseif pull_left
-                    knots[i+1][2] -= 1
-                elseif pull_up || pull_down
-                    knots[i+1][2] = knots[i][2]
-                end
+                drag_knot!(knots[i+1], knots[i])
             end
             amount -= 1
         end
